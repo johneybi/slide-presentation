40e25b331465d75a1a6a88f784d7869cd73a6f65
johneybi <eun082971@gmail.com>
2026-02-24 09:45:22 +0900

    fix/base-url

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/bitelearn-slide/",
+  base: "/slide-presentation/",
 });
